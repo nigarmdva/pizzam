8db21a94fdbdcebbcd7a025db90d53e537c248b0
nigarmdva <nigar.m@div.edu.az>
2025-05-04 00:54:10 +0400

    M

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -5,25 +5,33 @@ import UserProfileNav from "../Components/UserProfileNav";
 import ProductsPage from "../Layout/ProductsPage";
 import AuthLogin from "../Pages/Auth/login/AuthLogin";
 import { Register } from "../Pages/Auth/Register/Register";
-export const Router = createBrowserRouter([
-  {
-    path: "/register",
-    element: <Register />,
-  },
-  {
-    path: "/login",
-    element: <AuthLogin />,
-  },
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/user",
-    element: <UserPage />,
-  },
-  {
-    path: "/products",
-    element: <ProductsPage />,
-  },
-]);
+// export const Router = createBrowserRouter([
+//   
+// ]);
+
+import React from 'react'
+
+const router =createBrowserRouter([
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/login",
+        element: <AuthLogin />,
+      },
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/user",
+        element: <UserPage />,
+      },
+      {
+        path: "/products",
+        element: <ProductsPage />,
+      },
+]) 
+
+export default router
