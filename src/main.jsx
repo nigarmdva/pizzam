455de9b9e644fce2639e360926fa8657eccaf3cc
nigarmdva <nigar.m@div.edu.az>
2025-05-04 00:55:07 +0400

    M

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import router from "./router/Router"
+import router from "./router/router"
 createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
 );
