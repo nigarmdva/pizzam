a1f46609b04005eea70c4d90f2926e1f025ef4d5
nigarmdva <nigar.m@div.edu.az>
2025-05-04 10:47:21 +0400

    M

--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -3,14 +3,15 @@ import { useFormik } from "formik";
 import { axiosFunction } from "../api/index";
 import { signinSchema } from "../schemas/signInSchema";
 import useAuthStore from "../store/store";
-import { useNavigate } from 'react-router-dom';
-
+import { useNavigate } from "react-router-dom";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 export const SignIn = () => {
   const navigate = useNavigate();
 
   const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
   const { login } = useAuthStore();
 
@@ -27,17 +28,15 @@ export const SignIn = () => {
       try {
         const response = await axiosFunction("POST", "auth/login", values);
         if (response && response.accessToken) {
-          const user= response.user;
-          const accessToken= response.accessToken;
+          const user = response.user;
+          const accessToken = response.accessToken;
           login(user, accessToken);
-          navigate("/user"); 
+          navigate("/user");
         } else {
-
           throw new Error("Invalid response format");
         }
       } catch (apiError) {
         console.error("API call failed with error:", apiError);
-        console.error("Error details:", apiError.message);  
         if (apiError.response) {
           console.error("Response status:", apiError.response.status);
         }
@@ -54,7 +53,9 @@ export const SignIn = () => {
       <div className="flex justify-center items-center h-screen bg-gray-100 shadow-2xl shadow-black">
         <div className="bg-white p-8 sm:w-4/5 md:w-3/4 lg:w-1/2 w-full text-center">
           <h2 className="text-2xl font-bold text-gray-400">Xoş Gəldiniz!</h2>
-          <p className="text-gray-600 font-bold text-sm pt-1">Hesabınıza daxil olun</p>
+          <p className="text-gray-600 font-bold text-sm pt-1">
+            Hesabınıza daxil olun
+          </p>
           <div>
             <input
               value={formik.values.email}
@@ -65,44 +66,62 @@ export const SignIn = () => {
               type="email"
               placeholder="Email"
               className={`bg-gray-100 p-3 rounded-xl border-0 outline-none w-full sm:w-4/5 mt-3 ${
-                formik.errors.email && formik.touched.email ? "border-2 border-red-500" : ""
+                formik.errors.email && formik.touched.email
+                  ? "border-2 border-red-500"
+                  : ""
               }`}
             />
             {formik.errors.email && formik.touched.email && (
-              <p className="text-red-500 text-sm text-left pl-17">{formik.errors.email}</p>
+              <p className="text-red-500 text-sm sm:pl-17 text-left pl-0">
+                {formik.errors.email}
+              </p>
             )}
 
-            <input
-              id="password"
-              name="password"
-              value={formik.values.password}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              type="password"
-              placeholder="Password"
-              className={`bg-gray-100 p-3 rounded-xl border-0 outline-none w-full sm:w-4/5 mt-3 ${
-                formik.errors.password && formik.touched.password ? "border-2 border-red-500" : ""
-              }`}
-            />
+            <div className="relative w-full sm:w-4/5 mx-auto">
+              <input
+                id="password"
+                name="password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                type={showPassword ? "text" : "password"}
+                placeholder="Password"
+                className={`bg-gray-100 p-3 rounded-xl border-0 outline-none w-full mt-3 ${
+                  formik.errors.password && formik.touched.password
+                    ? "border-2 border-red-500"
+                    : ""
+                }`}
+              />
+              <div
+                className="absolute right-4 top-[50%] translate-y-[-50%] text-gray-500 cursor-pointer"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? <FaEyeSlash /> : <FaEye />}
+              </div>
+            </div>
             {formik.errors.password && formik.touched.password && (
-              <p className="text-red-500 text-sm text-left pl-17">{formik.errors.password}</p>
+              <p className="text-red-500 text-sm sm:pl-17 text-left">
+                {formik.errors.password}
+              </p>
             )}
           </div>
+
           {errorMessage && (
-            <p className="text-red-500 text-sm text-left pl-12 mt-3">{errorMessage}</p>
+            <p className="text-red-500 text-sm text-left pl-0 mt-3">
+              {errorMessage}
+            </p>
           )}
           <button
             type="submit"
             disabled={loading || formik.isSubmitting}
-            className="bg-[#000] text-white  cursor-pointer p-3 mt-8 rounded-xl w-full sm:w-2/5"
+            className="bg-[#000] text-white cursor-pointer p-3 mt-8 rounded-xl w-full sm:w-2/5"
           >
-            {loading ?"Daxil olunur..." : "Daxil ol"}
-            
+            {loading ? "Daxil olunur..." : "Daxil ol"}
           </button>
           <p className="text-gray-600 text-sm pt-1">
-            Hesabın yoxdur? 
+            Hesabın yoxdur?
             <a href="/register" className="underline">
-               Qeydiyyatdan Keç
+              Qeydiyyatdan Keç
             </a>
           </p>
         </div>
--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { useFormik } from "formik";
 import { signUpSchema } from "../schemas/signUpSchema";
 import { axiosFunction } from "../api/index";
 import useAuthStore from "../store/store";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
 export const SignUp = () => {
   const { login } = useAuthStore();
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  const togglePassword = () => setShowPassword((prev) => !prev);
+  const toggleConfirmPassword = () => setShowConfirmPassword((prev) => !prev);
 
   const { values, errors, touched, handleBlur, handleChange, handleSubmit } =
     useFormik({
@@ -41,67 +49,82 @@ export const SignUp = () => {
           <p className="text-gray-600 font-bold text-sm pt-1">
             Sifariş vermək üçün qeydiyyatdan keçin
           </p>
-          <div>
-            <input
-              value={values.email}
-              onChange={handleChange}
-              id="email"
-              type="text"
-              placeholder="Email"
-              className={`bg-gray-100 p-3 rounded-xl border-0 outline-0 w-full sm:w-[85%] mt-3 ${
-                errors.email && touched.email ? "border-2 border-red-500" : ""
-              }`}
-            />
-            {errors.email && touched.email && (
-              <p className="text-red-500 text-sm text-left pl-12">
-                {errors.email}
-              </p>
-            )}
-            <input
-              value={values.fullName}
-              type="text"
-              onChange={handleChange}
-              id="fullName"
-              placeholder="Ad Soyad"
-              className="bg-gray-100 p-3 rounded-xl border-0 outline-0 w-full sm:w-[85%] mt-3"
-            />
+
+          <input
+            value={values.email}
+            onChange={handleChange}
+            id="email"
+            type="text"
+            placeholder="Email"
+            className={`bg-gray-100 p-3 rounded-xl border-0 outline-0 w-full sm:w-[85%] mt-3 ${
+              errors.email && touched.email ? "border-2 border-red-500" : ""
+            }`}
+          />
+          {errors.email && touched.email && (
+            <p className="text-red-500 text-sm text-left pl-0 sm:pl-12">
+              {errors.email}
+            </p>
+          )}
+          <input
+            value={values.fullName}
+            type="text"
+            onChange={handleChange}
+            id="fullName"
+            placeholder="Ad Soyad"
+            className="bg-gray-100 p-3 rounded-xl border-0 outline-0 w-full sm:w-[85%] mt-3"
+          />
+          <div className="relative w-full sm:w-[85%] mx-auto mt-3">
             <input
               id="password"
               value={values.password}
               onChange={handleChange}
               onBlur={handleBlur}
-              type="password"
-              placeholder="Password"
-              className={`bg-gray-100 p-3 rounded-xl border-0 outline-0 w-full sm:w-[85%] mt-3 ${
+              type={showPassword ? "text" : "password"}
+              placeholder="Şifrə"
+              className={`bg-gray-100 p-3 pr-10 rounded-xl border-0 outline-0 w-full ${
                 errors.password && touched.password
                   ? "border-2 border-red-500"
                   : ""
               }`}
             />
-            {errors.password && touched.password && (
-              <p className="text-red-500 text-sm text-left pl-12">
-                {errors.password}
-              </p>
-            )}
+            <div
+              className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer text-gray-600"
+              onClick={togglePassword}
+            >
+              {showPassword ? <FaEyeSlash /> : <FaEye />}
+            </div>
+          </div>
+          {errors.password && touched.password && (
+            <p className="text-red-500 text-sm text-left pl-0 sm:pl-12">
+              {errors.password}
+            </p>
+          )}
+          <div className="relative w-full sm:w-[85%] mx-auto mt-3">
             <input
               id="confirmPassword"
               value={values.confirmPassword}
               onChange={handleChange}
               onBlur={handleBlur}
-              type="password"
-              placeholder=" Password"
-              className={`bg-gray-100 p-3 rounded-xl border-0 outline-0 w-full sm:w-[85%] mt-3 ${
+              type={showConfirmPassword ? "text" : "password"}
+              placeholder="Təkrar Şifrə"
+              className={`bg-gray-100 p-3 pr-10 rounded-xl border-0 outline-0 w-full ${
                 errors.confirmPassword && touched.confirmPassword
-                  ? "border-2 border-red-500 text-left pl-12"
+                  ? "border-2 border-red-500"
                   : ""
               }`}
             />
-            {errors.confirmPassword && touched.confirmPassword && (
-              <p className="text-red-500 text-sm text-left pl-12">
-                {errors.confirmPassword}
-              </p>
-            )}
+            <div
+              className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer text-gray-600"
+              onClick={toggleConfirmPassword}
+            >
+              {showConfirmPassword ? <FaEyeSlash /> : <FaEye />}
+            </div>
           </div>
+          {errors.confirmPassword && touched.confirmPassword && (
+            <p className="text-red-500 text-sm text-left pl-0 sm:pl-12">
+              {errors.confirmPassword}
+            </p>
+          )}
           <button
             type="submit"
             className="bg-[#000] text-white p-3 mt-8 rounded-xl w-full sm:w-[45%]"
